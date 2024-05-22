b3951fd69c968d008312c8244f31bfb8dd2bde6b
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-05-22 17:05:09 +0530

    added the OOPS approach, and learn bind method supremacy

--- script.js
+++ script.js
@@ -15,91 +15,98 @@ console.log(navigator.geolocation);
 let map;
 let mapEvent;
 
+class App{
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
+  #map;
+  #mapEvent;
+
+  constructor(){
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    
+    inputType.addEventListener('change', this._toggleElevationField);
+
+  }
+
+  _getPosition(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+       this._loadMap.bind(this)
+        ,
+        function () {
+          console.log('not able to fetched the coordinates ');
+        }
+      );
+    }
+  }
+
+  _loadMap(position) {
+    
       console.log(position.coords);
 
       const { longitude } = position.coords;
       const { latitude } = position.coords;
 
       const location = [latitude, longitude];
-      // console.log(
-      //   `https://www.google.com/maps/@${longitude},${latitude}?entry=ttu`
-      // );
-
-      // const map = L.map('map').setView(coords, 13);
-      map = L.map('map').setView(location, 13);
+     
+     this.#map = L.map('map').setView(location, 13);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+      }).addTo(this.#map);
 
 
 
       // instead of using eventlistener to add marker on map , we are using the leaflet provided method to handle that 
 
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-
-
-        //   console.log(mapEvent);
-        //   const {lat , lng} = mapEvent.latlng;
-        //   // console.log(L);
-        //   L.marker([lat,lng])
-        // .addTo(map)
-        // .bindPopup(
-        //   L.popup({
-        // maxWidth: 250,
-        // minWidth : 100,
-        // autoClose : false,
-        // closeOnClick : false,
-        // className : 'running-popup'
-
-        // })).setPopupContent("Workout")
-        // .openPopup();
-
-
-      });
-    },
-    function () {
-      console.log('not able to fetched the coordinates ');
+      this.#map.on('click', this._showForm.bind(this) );
     }
-  );
+  
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+
+    
+    e.preventDefault();
+    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+  
+  
+    console.log(this.#mapEvent);
+    const { lat, lng } = this.#mapEvent.latlng;  // why not here we change as #mapEvent ???
+    // console.log(L);
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup'
+  
+        })).setPopupContent("Workout")
+      .openPopup();
+  }
 }
+
+const app = new App();
+
+
 
 
 // handling separetly that when user click on the submit 
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
 
-
-  console.log(mapEvent);
-  const { lat, lng } = mapEvent.latlng;
-  // console.log(L);
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup'
-
-      })).setPopupContent("Workout")
-    .openPopup();
-
-});
-
-inputType.addEventListener('change',function(){
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-})
 
